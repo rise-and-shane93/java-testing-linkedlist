d5034e6178b66771d887ecfca3096aae8c3e6a96
Shane Harper <cdtharper@gmail.com>
2021-04-18 14:31:38 -0400

    first commit

---
+++ Job.java
@@ -0,0 +1,9 @@
+public class Job {
+    private int duration;
+    private int jobNumber;
+
+    public Job(int duration, int jobNumber) {
+        this.duration = duration;
+        this.jobNumber = jobNumber;
+    }
+}
---
+++ PrintJobs.java
@@ -0,0 +1,5 @@
+public class PrintJobs {
+    public static void main(String[] args) {
+        
+    }
+}
